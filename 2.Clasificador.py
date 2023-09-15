291c36473c6697a949abd757f1b0cc8aef44b3fb
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-09-15 11:44:38 +0200

    Delete 2.Clasificador.py

--- 2.Clasificador.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-datos, metadatos = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
-
-datos_entrenar, datos_test = datos['train'], datos['test'] 
-
-nombre_clases= metadatos.features['label'].names
